de5618ae136dfd82df526cf5ae3dd7e2ca277a75
slymachenko <senya.s408@gmail.com>
2026-05-26 15:13:14 +0200

    NEW: exploration done; config updated

--- common/common/config.py
+++ common/common/config.py
@@ -1,9 +1,17 @@
 from pathlib import Path
 
+# PATHS
 ROOT_DIR = Path(__file__).resolve().parent.parent.parent
 
 ASSETS_PATH = ROOT_DIR / "assets"
 FIGS_PATH = ASSETS_PATH / "figs"
 
 DATA_DIR = ROOT_DIR / "data"
-MNIST_PATH = DATA_DIR / "MNIST.npz"
+DATA_RAW_DIR = DATA_DIR / "raw"
+MNIST_RAW_PATH = DATA_RAW_DIR / "MNIST.npz"
+
+DATA_PREPROCESSED_DIR = DATA_DIR / "preprocessed"
+MNIST_PREPROCESSED_PATH = DATA_PREPROCESSED_DIR / "MNIST_preprocessed.npz"
+
+# SEED
+SEED = 42
