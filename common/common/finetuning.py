f6de01bbda5204d9ca61b8c866ace4b9d524c56d
slymachenko <senya.s408@gmail.com>
2026-06-07 12:18:18 +0200

    UPD: added return of history for optimize_via_zooming_grid

--- common/common/finetuning.py
+++ common/common/finetuning.py
@@ -1,6 +1,7 @@
 import numpy as np
-from typing import Callable, Tuple, Dict, Any
 from sklearn.metrics import average_precision_score, precision_recall_curve
+
+from typing import Callable, Tuple, Dict, Any, List
 
 def optimize_via_zooming_grid(
     score_fn: Callable[[float], Tuple[np.ndarray, np.ndarray]],
@@ -9,13 +10,19 @@ def optimize_via_zooming_grid(
     size: int = 4,
     depth: int = 1,
     param_name: str = "Param",
+    is_integer: bool = False,
     verbose: bool = False
-) -> Tuple[float, float]:
+) -> Tuple[float, float, Dict[str, List[Any]]]:
     """Finds the hyperparameter value that maximizes PR AUC."""
     current_min = param_min
     current_max = param_max
     global_best_auc = -1.0
     global_best_param = None
+    history = {
+        "zoom_step": [],
+        param_name: [],
+        "auc": []
+    }
 
     for step in range(depth):
         if verbose:
@@ -23,11 +30,13 @@ def optimize_via_zooming_grid(
             print(f"{param_name:<10} | {'PR AUC':<10}")
 
         grid = np.linspace(current_min, current_max, size)
+        if is_integer:
+            grid = np.unique(np.round(grid).astype(int))
         local_best_auc = -1.0
-        local_best_param = None 
+        local_best_param = None
 
         for param_val in grid:
-            param_val = float(param_val)
+            param_val = int(param_val) if is_integer else float(param_val)
             val_normal_scores, val_anomaly_scores = score_fn(param_val)
 
             y_true = np.concatenate([np.zeros_like(val_normal_scores, dtype=int), np.ones_like(val_anomaly_scores, dtype=int)])
@@ -37,6 +46,10 @@ def optimize_via_zooming_grid(
 
             if verbose:
                 print(f"{param_val:<10.4f} | {auc:<10.4f}")
+
+            history["zoom_step"].append(step + 1)
+            history[param_name].append(param_val)
+            history["auc"].append(float(auc))
 
             if auc > local_best_auc:
                 local_best_auc = auc
@@ -50,7 +63,10 @@ def optimize_via_zooming_grid(
         current_min = np.clip(local_best_param - (step_size / 2), param_min, param_max)
         current_max = np.clip(local_best_param + (step_size / 2), param_min, param_max)
 
-    return global_best_param, global_best_auc
+        if is_integer:
+            current_min, current_max = np.floor(current_min), np.ceil(current_max)
+
+    return global_best_param, global_best_auc, history
 
 def optimize_fbeta_threshold(
     val_normal_scores: np.ndarray, 
